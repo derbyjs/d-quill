c200ba3fc7463410ecb73638dc705f7722984146
John R. Brewster <John.Brewster@GTRI.gatech.edu>
2014-11-04 13:16:01 -0500

    Updates for newer quillJs
    Changes to delta, updates for using more of the Authorship and a
    greater toolbar.

--- index.js
+++ index.js
@@ -3,37 +3,169 @@ var Range = require('quilljs/lib/lib/range');
 var LINE_FORMATS = {
   'align': true
 };
-var BINARY_FORMATS = {
-  'bold': true
-, 'italic': true
-, 'strike': true
-, 'underline': true
+
+//
+//  1 Magenta
+//  2 aqua
+//  3 Yellow
+//  4 Grey
+//
+
+// Add Authors here  hardcode now, dynamic later.
+//
+var AUTHORCOLORS = {
+  "":"rgb(119,136,153)"
 };
+
+
+var BINARY_FORMATS = {
+  'bold': true,
+  'italic': true,
+  'strike': true,
+  'underline': true,
+  'font': true,
+  'size':true,
+  'color': true,
+  'background': true,
+  'image': true,
+  'link': true,
+  'bullet': true,
+  'list': true,
+  'align':true,
+  'author':true,
+  'multi-cursor':true
+};
+
 var MIXED_FORMAT_VALUE = '*';
 
 module.exports = DerbyQuill;
-function DerbyQuill() {}
-DerbyQuill.prototype.view = __dirname;
+function DerbyQuill() {};
+
+
 
 DerbyQuill.prototype.init = function() {
   this.quill = null;
   this.activeFormats = this.model.at('activeFormats');
   this.value = this.model.at('value');
+  this.toolbar = this.model.at('toolbar');
 };
 
-DerbyQuill.prototype.create = function() {
-  var Quill = require('quilljs');
-  var quill = this.quill = new Quill(this.editor);
+DerbyQuill.prototype.view = __dirname;
 
+DerbyQuill.prototype.create = function() {
   var self = this;
-  quill.on('text-change', function() {
-    self.value.set(quill.editor.innerHTML);
+
+  var _author = self.page.params.url.substring(1);
+
+  var Quill = require('quilljs');
+  var options = {};
+    options.theme = "snow";
+    options.modules = {
+      "link-tooltip": true,
+      "toolbar": {"container":'#toolbar'},
+      'image-tooltip': true,
+      'multi-cursor': true
+      };
+
+  if(_author) {
+    options.modules["authorship"] = {authorId: _author, enabled: true};
+  }
+
+  var quill = this.quill = new Quill(this.editor, options);
+
+  if(_author) {
+    var authorship = this.quill.getModule("authorship");
+    authorship.addAuthor(_author, AUTHORCOLORS[_author]);
+  }
+
+  var cursorship = this.quill.getModule("multi-cursor");
+  cursorship.setCursor(_author, this.quill.getLength()-1, _author, AUTHORCOLORS[_author]);
+
+  // Hack - to get the HTML into the Quill Editor
+  // and not just the view.
+  this.quill.setHTML(this.model.data.value);
+  var model = this.model;
+
+
+
+
+  // THe Model has changed.  THis means ANOTHER quill connected to this
+  // thing has changed the data.  Update the quill editor.
+  //
+  model.on("change", "value", function(newVal, oldVal, passed){
+    self.quill.setHTML(newVal);
+  });
+
+  self.dom.on('click', function (mouseEvent) {
+    if( "Toggle Author" == mouseEvent.target.title) {
+      var a = self.quill.getModule("authorship");
+          a.enable();
+    }
+  });
+
+  var getDeltaEnd = function (delta) {
+    for (var index in delta.ops){
+      for (var name in delta.ops[index]) {
+        switch(name){
+          case "insert":
+          case "delete":
+              break;
+          case "retain":
+              return delta.ops[index][name];
+              break;
+        }
+
+      }
+    }
+  }
+
+  var getDeltaAuthor = function (delta) {
+    for (var index in delta.ops){
+      for (var name in delta.ops[index]) {
+        switch(name){
+          case "insert":
+          case "delete":
+          case "retain":
+              break;
+          case "attributes":
+              return delta.ops[index].attributes.author;
+        }
+
+      }
+    }
+
+  }
+
+  // this current user has changed or the model has changed.
+  // need to check which one "user" or "api" made the change
+  //
+  quill.on('text-change', function(delta, source) {
+    var _end = getDeltaEnd(delta);
+
+    if(source == "user"){
+      self.value.set(quill.editor.innerHTML);
+
+    cursorship.moveCursor(_author, _end);
+    _end++;
+      self.quill.setSelection(_end, _end);
+    }
+
+    // else {
+    //
+    //   var _author = getDeltaAuthor(delta);
+    //
+    //   cursorship.moveCursor(_author, _end);
+    // }
+
     var range = quill.getSelection();
     self.updateActiveFormats(range);
   });
-  quill.on('selection-change', function(range) {
+
+
+  quill.on('selection-change', function(range, source) {
     self.updateActiveFormats(range);
   });
+
   // HACK: Quill should provide an event here, but we wrap the method to
   // get a hook into what's going on instead
   var prepareFormat = quill.prepareFormat;
@@ -41,6 +173,7 @@ DerbyQuill.prototype.create = function() {
     prepareFormat.call(quill, name, value);
     self.activeFormats.set(name, value);
   };
+
   // Iframes will stop bubbling at their window, so re-dispatch all clicks
   // that bubble to the top of the iframe document on the containing element.
   // This is helpful for popups to figure out when they should close
@@ -48,6 +181,7 @@ DerbyQuill.prototype.create = function() {
     var event = new MouseEvent(e.type, e);
     self.editor.dispatchEvent(event);
   });
+
 };
 
 DerbyQuill.prototype.clearFormatting = function() {
@@ -161,8 +295,8 @@ DerbyQuill.prototype.setRangeContents = function(range, value, attributes) {
   var startLength = this.quill.getLength();
   this.quill.setContents({
     startLength: startLength
-  , ops: [
-      {start: 0, end: range.start}
+    , ops: [
+    {start: 0, end: range.start}
     , {value: value, attributes: attributes}
     , {start: range.end, end: startLength}
     ]
