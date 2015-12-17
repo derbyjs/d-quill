cbefd794df339e929ca0cca04236e3cc29a31ba0
Zeus <lalkaka.zeus@gmail.com>
2015-12-16 16:59:14 -0800

    fallback to set instead of setDiffDeep to avoid odd bug [0]

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ DerbyQuill.prototype._updateDelta = function() {
   var pass = {source: this.quill.id};
   // TODO: Change to setDiffDeep once we figure out the error
   // shown here: https://lever.slack.com/files/jon/F0GH44U74/screen_shot_2015-12-13_at_3.00.37_pm.png
-  this.delta.pass(pass).setDiffDeep(deepyCopy(this.quill.editor.doc.toDelta()));
+  this.delta.pass(pass).set(deepyCopy(this.quill.editor.doc.toDelta()));
 }
 
 DerbyQuill.prototype.clearFormatting = function() {
